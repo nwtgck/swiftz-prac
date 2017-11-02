be62a3bbe6e49e74b485375f4a5fef3d9f9f632b
Ryo Ota <nwtgck@gmail.com>
2017-11-03 07:55:27 +0900

    [add] Recomment-out some code in ListPrac

--- Sources/ListPrac.swift
+++ Sources/ListPrac.swift
@@ -1,6 +1,10 @@
 // (from: https://github.com/typelift/Swiftz)
 
 import struct Swiftz.List
+// import Swiftz
+// import struct Swiftz.Function
+// import protocol Swiftz.Category
+import Swiftz
 
 // List practice
 func listPrac(){
@@ -17,11 +21,22 @@ func listPrac(){
 
     let twoToEleven = l.map{$0 + 1} // [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     print("twoToEleven:", twoToEleven)
-    // let even = l.filter((==0) • (%2)) // [2, 4, 6, 8, 10]
-    // let sum = l.reduce(curry(+), initial: 0) // 55
+    // let g : (Int) -> Int  = {$0 % 2}
+    // let f : (Int) -> Bool = {$0==0}
+    // let h  = f • g
+    // let g : Function<Int, Int>  = Function({$0 % 2})
+    // let f : Function<Int, Bool> = Function({$0==0})
+    // let h  = f • g
 
-    // //: Plus a few more.
-    // let partialSums = l.scanl(curry(+), initial: 0) // [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
-    // let firstHalf = l.take(5) // [1, 2, 3, 4, 5]
-    // let lastHalf = l.drop(5) // [6, 7, 8, 9, 10]
+    // let even = l.filter({$0==0} • {$0 % 2}) // [2, 4, 6, 8, 10]
+    let sum = l.reduce(curry(+), initial: 0) // 55
+    print("sum:", sum)
+
+    //: Plus a few more.
+    let partialSums = l.scanl(curry(+), initial: 0) // [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+    print("partialSums:", partialSums)
+    let firstHalf = l.take(5) // [1, 2, 3, 4, 5]
+    let lastHalf = l.drop(5) // [6, 7, 8, 9, 10]
+    print("firstHalf:", firstHalf)
+    print("lastHalf:", lastHalf)
 }
